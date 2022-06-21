f9c13be5ad63b4ddc6668b01c7298733a37abef0
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-06-21 22:02:25 +0100

    Melhorando o ambiente de desenvolvimento no VS Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <h1>Hello World</h1>
     </div>
   )
 }
-
 export default App
