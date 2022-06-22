3726a2d6b7da8e48504d70a9ec333550db2b001e
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-06-22 08:39:52 +0100

    Codando de verdade

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
-import {gql, useQuery} from "@apollo/client"
-import {useEffect} from "react"
-import {client} from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql`
-  query{
-      lessons {
-        id
-        title
-        teacher{
-          name
-        }
-      }
-  }
-`
-interface Lesson{
-  id: string;
-  title: string;
-}
-
 function App() {
-   const {data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
-
-   console.log(data)
   return (
     <div className="App">
-      <h1 className="text-10xl text-violet-600">Hello World</h1>
-      <ul>
-        {data?.lessons.map(lesson=>{
-          return <li key={lesson.id}>{lesson.title} </li>
-        })}
-      </ul>
+     
     </div>
   )
 }
