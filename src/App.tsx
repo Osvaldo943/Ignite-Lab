b47e583cca894f979b94811fee5cec45bc0c4f62
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-06-22 09:01:37 +0100

    Criando Estrura da pasta componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import {Header} from "./components/Header"
+import {Sidebar} from "./components/Sidebar"
+import {Lesson} from "./components/Lesson"
+import {Video} from "./components/Video"
+
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      <Lesson />
+      <Sidebar />
+      <Video />
     </div>
   )
 }
