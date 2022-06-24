6c777df907cb8bcbc2dbee5afdd6a1d7f85e9fba
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-06-24 10:31:33 +0100

    Video improved

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,22 @@
 export function Video(){
   return(
     <div className="video">
-      Video
+      <div className="video-container-black">
+        <div className="video-content">  asasa </div>
+      </div>
+
+
+      <div className="video-rest">
+        <div className="title-desc">
+          <div className="text-desc">
+            <h1>Aula 01 -Abertura do Ignite-Lab</h1>
+            <p>Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.</p>
+          </div>
+          <div>
+            <a href="" className="text-sm rounded  gap-2">Comunidade do Discord</a>
+          </div>
+         </div>
+      </div>
     </div>
   )
 }
