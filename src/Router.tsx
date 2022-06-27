fb0ba0e95206d5f9714d450161fc825072f72c42
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-06-27 14:33:19 +0100

    Images added in subscribe

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 
 import { Event } from "./pages/event";
+import { Subscribe } from "./pages/subscribe";
 export function Router(){
   return(
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={<Subscribe />} />
       <Route path="/event" element={<Event/>} />
       <Route path="/event/lesson/:slug" element={<Event/>} />
     </Routes>
