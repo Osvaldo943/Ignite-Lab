09625971c884fd74ab3399680e3419b58479d0ec
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-06-27 14:57:03 +0100

    Text in subscribe added

--- src/pages/subscribe.tsx
+++ src/pages/subscribe.tsx
@@ -1,8 +1,18 @@
+import codeGroup from "../assets/code-group.png"
+import { Logo } from "../components/Logo"
+
 export function Subscribe(){
   return(
     <div className="subscribe">
+      <div className="hero-subscribe">
+        <div className="text-side-subscribe">
+        <Logo />
+        <h1>Construa uma <strong>aplicação complete</strong> do zero, com <strong>React</strong></h1>
+        <p>Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.</p>
+        </div>
+      </div>
       <div>
-        subscribe
+        <img src={codeGroup} alt="" />
       </div>
     </div>
   )
