32d820a46f733d2589a9ae3381c869e5e997cd18
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-06-27 15:51:36 +0100

    Added form in subscribe

--- src/pages/subscribe.tsx
+++ src/pages/subscribe.tsx
@@ -5,14 +5,23 @@ export function Subscribe(){
   return(
     <div className="subscribe">
       <div className="hero-subscribe">
-        <div className="text-side-subscribe">
+        <div className="text-side-subscribe ">
           <div className="logo-subscribe">
           <Logo />
           </div>
-        <h1>Construa uma <strong>aplicação complete</strong> ,do zero, com <strong>React</strong></h1>
+        <h1>Construa uma <strong>aplicação complete</strong> do zero, com <strong>React</strong></h1>
         <p>Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.</p>
         </div>
+        <div className="form-subsccribe">
+        <strong>Inscreva-se gratuitamente</strong>
+        <form action="">
+          <input type="text" placeholder="Seu nome completo" />
+          <input type="text" placeholder="Digite seu email" />
+          <button type="submit">Garantir minha vaga</button>
+        </form>
       </div>
+      </div>
+     
       <div>
         <img src={codeGroup} alt="" />
       </div>
