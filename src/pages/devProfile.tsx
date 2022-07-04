f3759eb72081da09be69616446b6b29aa3fe37c6
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-07-04 22:35:14 +0100

    some points in devProfile added

--- src/pages/devProfile.tsx
+++ src/pages/devProfile.tsx
@@ -5,6 +5,8 @@ export function DevFunction(){
     <div>
       <p>Perfil do desenvolver</p>
       <h1>Osvaldo de Sousa</h1>
+      <p>Dline-Code</p>
+      <p>Frontend developer in dline code</p>
     </div>
   )
 }
