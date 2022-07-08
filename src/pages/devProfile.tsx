a896bfe833c8a3a113436375518ea788178d9be4
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-07-08 11:52:39 +0100

    Dev profile left side added

--- src/pages/devProfile.tsx
+++ src/pages/devProfile.tsx
@@ -1,11 +1,40 @@
-import React from "react"
+import { Barbell, EyedropperSample, GithubLogo, LinkedinLogo } from "phosphor-react"
+import AvatarProfile from "../assets/avatar-profile.jpg"
 
 export function DevProfile(){
   return(
     <div className="dev-profile">
       <div className="container">
-        <div className="right-left">
-          
+        <div className="left-profile">
+          <div className="image-profile">
+            <img src={AvatarProfile} alt="Osvaldo's Avatar" />
+          </div>
+          <ul className="dev-info-profile">
+            <li>
+              <span><Barbell /></span>
+              <h2>Osvaldo de Sousa</h2>
+            </li>
+            <li>
+              <span><EyedropperSample /></span>
+              <h2>Frontend developer</h2>
+            </li>
+            <li>
+              <span><Barbell /></span>
+              <h2>Member of DLine-Code</h2>
+            </li>
+            <li>
+              <span><address /></span>
+              <h2>Angola, Luanda</h2>
+            </li>
+            <li>
+              <span><GithubLogo /></span>
+              <h2>https://github.com/Osvaldo943</h2>
+            </li>
+            <li>
+              <span><LinkedinLogo /></span>
+              <h2>https://www.linkedin.com/in/osvaldo-de-sousa-01a937206/</h2>
+            </li>
+          </ul>
         </div>
         <div className="right-profile">Left side</div>
       </div>
