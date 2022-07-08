0c1295e62b88f37060e2cef72fcb640367e2f9c2
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-07-08 11:54:48 +0100

    Dev profile added in Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-
+import { DevProfile } from "./pages/devProfile";
 import { Event } from "./pages/event";
 import { Subscribe } from "./pages/subscribe";
+
 export function Router(){
   return(
     <Routes>
       <Route path="/" element={<Subscribe />} />
       <Route path="/event" element={<Event/>} />
       <Route path="/event/lesson/:slug" element={<Event/>} />
+      <Route path="/devProfile" element={<DevProfile/>} />
     </Routes>
   );
 }
