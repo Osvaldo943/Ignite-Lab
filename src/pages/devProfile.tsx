67f3e35c7c588c316e98ea82360a87659a3e306e
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-07-08 11:42:21 +0100

    Dev profile structure created

---
+++ src/pages/devProfile.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export function DevProfile(){
+  return(
+    <div className="dev-profile">
+      <div className="container">
+        <div className="right-left">
+          
+        </div>
+        <div className="right-profile">Left side</div>
+      </div>
+    </div>
+  )
+}
