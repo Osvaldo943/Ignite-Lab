dcb9d1f7d054c676fcad267d3f40726cb4e510c8
Osvaldo943 <osvaldodesousa9432@gmail.com>
2022-07-08 13:15:03 +0100

    Profile improved

--- src/pages/devProfile.tsx
+++ src/pages/devProfile.tsx
@@ -1,5 +1,6 @@
-import { Barbell, EyedropperSample, GithubLogo, LinkedinLogo } from "phosphor-react"
+import { Square,AddressBook, Barbell, Code, EyedropperSample, FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo, PhoneCall } from "phosphor-react"
 import {Header} from "../components/Header"
+import {Link} from "react-router-dom"
 import AvatarProfile from "../assets/avatar-profile.jpg"
 
 export function DevProfile(){
@@ -10,43 +11,69 @@ export function DevProfile(){
         <div className="left-profile">
           <div className="image-profile">
             <img src={AvatarProfile} alt="Osvaldo's Avatar" />
+            <span>Osvaldo de Sousa</span>
+            <span>Front-End</span>
           </div>
           <ul className="dev-info-profile">
             <li>
-              <span><Barbell /></span>
-              <h2>Osvaldo de Sousa</h2>
+              <Link to="">
+                <span><GithubLogo /></span>
+                <p>Visit my GitHub</p>
+              </Link>
             </li>
             <li>
-              <span><EyedropperSample /></span>
-              <h2>Frontend developer</h2>
+              <Link to="">
+                <span><LinkedinLogo /></span>
+                <p>Visit my linkdein</p>
+              </Link>
             </li>
             <li>
-              <span><Barbell /></span>
-              <h2>Member of DLine-Code</h2>
+              <Link to="">
+                <span><FacebookLogo /></span>
+                <p>Visit my Facebook</p>
+              </Link>
             </li>
             <li>
-              <span><address /></span>
-              <h2>Angola, Luanda</h2>
-            </li>
-            <li>
-              <span><GithubLogo /></span>
-              <h2>https://github.com/Osvaldo943</h2>
-            </li>
-            <li>
-              <span><LinkedinLogo /></span>
-              <h2>https://www.linkedin.com/in/osvaldo-de-sousa-01a937206/</h2>
+              <Link to="">
+                <span><InstagramLogo /></span>
+                <p>Visit my Instagram</p>
+              </Link>
             </li>
           </ul>
         </div>
         <div className="right-profile">
           <div className="dev-desc-profile">
-            <p>Left sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft sideLeft side</p>
+            <p>Osvaldo de Sousa is my name, front-end developer in DLine-Code. 
+              I'm focused on JavaScript and I like building projects that 
+              challenge myself. For doing my projects I often use: {"{React, TypeScript, TailWindCSS, React Native and Node}"}.
+              <br/>
+              I love doing what I do and I'm building my career.
+            </p>
+            <div className="dev-contact">
+              <span><i><Square /> </i> Angola, Luanda</span>
+              <span><i><PhoneCall /> </i> 943-942-499</span>
+              <span><i><Code /> </i> DLine-Code</span>
+            </div>
           </div>
           <ul className="dev-carroussels-skills">
-            <li>Icon</li>
-            <li>Icon</li>
-            <li>Icon</li>
-            <li>Icon</li>
+            <li>
+              <Link to="">HMTL</Link>
+            </li>
+            <li>
+              <Link to="">CSS</Link>
+            </li>
+            <li>
+              <Link to="">JavaScript</Link>
+            </li>
+            <li>
+              <Link to="">React</Link>
+            </li>
+            <li>
+              <Link to="">Next JS</Link>
+            </li>
+            <li>
+              <Link to="">TypeScript</Link>
+            </li>
           </ul>
         </div>
       </div>
